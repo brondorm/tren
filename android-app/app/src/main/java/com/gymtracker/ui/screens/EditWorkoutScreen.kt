ccfe3b8bbc48879cab8e685a7a54a0bcd7f18744
brondorm <91697203+brondorm@users.noreply.github.com>
2025-11-29 13:57:14 +0300

    Add import for clickable modifier in EditWorkoutScreen

--- android-app/app/src/main/java/com/gymtracker/ui/screens/EditWorkoutScreen.kt
+++ android-app/app/src/main/java/com/gymtracker/ui/screens/EditWorkoutScreen.kt
@@ -1,5 +1,6 @@
 package com.gymtracker.ui.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
